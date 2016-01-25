a5228a482fbb0d09a2cbed4fb97751c15ae521e5
lexthatch <lexthatch@hotmail.com>
2016-01-24 23:13:32 -0700

    rest of it
    stuff to make it work

---
+++ app.js
@@ -0,0 +1,59 @@
+<script>
+
+var rock;
+var paper;
+var scissors;
+
+function playerChoice() {
+
+}
+
+function computerChoice() {
+    var computerChoice = Math.random();
+    if (computerChoice <= 0.33) {
+        computerChoice = "rock";
+        return "rock";
+    }
+    else if (0.34 <= computerChoice <= 0.66) {
+        computerChoice = "paper";
+        return "paper";
+    }
+    else {
+        computerChoice = "scissors";
+        return "scissors";
+    }
+
+    
+
+}
+function play() {
+    pc = playerChoice;
+    cc = computerChoice;
+
+    if (pc === cc) {
+        alert("tie");
+    }
+    else if (pc === "rock" && cc === "scissors") {
+        alert("You win.");
+    }
+    else if (pc === "paper" && cc === "rock") {
+        alert("You win.");
+    }
+    else if (pc === "scissors" && cc === "paper") {
+        alert("You win.");
+    }
+    else if (pc === "scissors" && cc === "rock") {
+        alert("Computer wins. Too bad.");
+    }
+    else if (pc === "rock" && cc === "paper") {
+        alert("Computer wins. Too bad.");
+    }
+    else if (pc === "paper" && cc === "scissors") {
+        alert("Computer wins. Too bad.");
+    }
+
+
+
+    </script>
+
+  
